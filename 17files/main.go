56f1f075cc08f349ec963e6e678f67f5c077589d
shubham kumar <shubhamkmuz@gmail.com>
2025-12-15 18:12:29 +0530

    files in go

--- 17files/main.go
+++ 17files/main.go
@@ -14,15 +14,40 @@ func main() {
 	content := "this is a sample file created using GoLang"
 
 	file, err := os.Create("./mylcogofile.txt")
-	if err != nil {
+	/*if err != nil {
 		panic(err)
-	}
+	}*/
+	checkNilError(err)
 
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	checkNilError(err)
 	fmt.Println("Length is: ", length)
 
 	defer file.Close()
+
+	readFile("./mylcogofile.txt")
+}
+
+func readFile(filename string) {
+	fmt.Println("Reading File")
+
+	// way 1 to read file
+	/* dataBytes, err := ioutil.ReadFile("./mylcogofile.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Text data inside file is: \n", string(dataBytes)) */
+
+	// way 2 to read file
+	// ioutil is deprecated
+	// use os package instead
+	dataBytes, err := os.ReadFile(filename)
+	checkNilError(err)
+	fmt.Println("Text data inside file is: \n", string(dataBytes))
+}
+
+func checkNilError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
